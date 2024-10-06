69933b377742432a4022e2e7d52a1ffd1904bdc4
Zrrg <hiroaritillwhen@gmail.com>
2024-10-06 03:34:53 +0300

    fix: read with promises

--- src/fs/read.js
+++ src/fs/read.js
@@ -3,16 +3,11 @@ const path = './src/fs/files/'
 const file = 'fileToRead.txt'
 
 const read = async (source) => {
-    try {
+
     const data = await fs.promises.readFile(source, 'utf8') 
-        try {
-            console.log(data)
-        } catch (error) {
-            console.error("FS Operation failed", error)
-        }
-    } catch {
-        console.error("FS Operation failed")
-    }
+        .then((data) => console.log(data))
+        .catch((error) => console.error("FS Operation failed", error))
+
 }
 
 
