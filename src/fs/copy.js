db55b1c7d8d059e5aac6dd7c04583b410ab65efa
Zrrg <hiroaritillwhen@gmail.com>
2024-10-06 03:16:31 +0300

    fix: copy with promises

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -7,17 +7,16 @@ const destination = './src/fs/files_copy'
 // throws error if source don't exist by default
 // error on exist throws error if destination exists
 
-const copy = async () => {
-        fs.cp(source, destination, {force:false, errorOnExist:true, recursive:true }, (err) => {
-            if (err) {
-                console.error("FS operation failed", err)
-            } else {
+const copy = async (src, dest) => {
+        fs.promises.cp(src, dest, {force:false, errorOnExist:true, recursive:true })
+            .then( () => {
                 console.log("Copied")
-            }
-        } )
-    
-    }
+            })
+            .catch( (err) => {
+                console.error("FS operation failed", err)
+            })
+        }
+             
 
 
-
-await copy();
+await copy(source, destination);
